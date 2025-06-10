d5e9427a780db1dc6790cf5ae4204ceeae7571ad
DoThanhNhan <tnhan.ittechnology@gmail.com>
2025-06-10 09:00:18 +0700

    init commit test 2

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,15 +1,14 @@
-import { Router } from 'express';
-import { UserController } from '../controllers/UserController';
+import { Router } from "express";
+import { UserController } from "../controllers/UserController";
 
 const router = Router();
 const userController = new UserController();
 
 export const setUserRoutes = (app: any) => {
-    app.use('/api/users', router);
+  app.use("/api/users", router);
 
-    router.post('/', userController.createUser.bind(userController));
-    router.get('/:id', userController.getUser.bind(userController));
-    
-    // Add this route for /theloai
-    app.get('/theloai', userController.getTheLoai.bind(userController));
+  router.post("/", userController.createUser.bind(userController));
+  router.get("/:id", userController.getUser.bind(userController));
+
+  app.get("/theloai", userController.getTheLoai.bind(userController));
 };
