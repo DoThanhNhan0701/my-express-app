c9cb14cd589332cc7bbd1e97a930d45b20fa2994
DoThanhNhan <tnhan.ittechnology@gmail.com>
2025-06-10 09:16:37 +0700

    style: standardize quotes and update API title in swagger.ts

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,18 +1,16 @@
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'My Express API',
-      version: '1.0.0',
+      title: "Doc api v1",
+      version: "1.0.0",
     },
   },
-  apis: ['./src/controllers/*.ts'],
+  apis: ["./src/controllers/*.ts"],
 };
 
 export const swaggerSpec = swaggerJSDoc(options);
 export { swaggerUi };
-
-
